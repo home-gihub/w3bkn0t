2724ba915842429cf16da6e9fccf03b8f56dc372
home-github <124263428+home-gihub@users.noreply.github.com>
2023-07-15 20:45:04 +0100

    add back to home

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const card = document.getElementById('card')
+function reloadbtoh() {
+    location.reload()
+}
 var file = ""
 const fileInput = document.getElementById('ipain');
 fileInput.addEventListener('change', (event) => {
@@ -6,11 +9,16 @@ fileInput.addEventListener('change', (event) => {
     console.log(file[0]);
   });
 
+function exploit() {
+    console.log('the exploit should run now but there is not anything here')
+}
+
 function run() {
     card.innerHTML = '<p class="big">Working.</p>'
     if (file) {
         card.innerHTML = '<p class="big">Running Exploit.</p>'
+        exploit()
     } else {
-        card.innerHTML = '<p class="big"> ERROR: No File Seclected. </p>'
+        card.innerHTML = '<p class="big"> ERROR: No File Seclected. </p> <br> <button onclick="reloadbtoh();"> <p>Back to home</p> </button>'
     }
 }
