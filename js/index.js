ff25b3d456d904c9ca7f9e3de126ab41a0823b1b
home-github <124263428+home-gihub@users.noreply.github.com>
2023-07-15 20:55:57 +0100

    fix styles and fix ismobile

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ function reloadbtoh() {
 }
 var file = ""
 const isMobile = navigator.userAgentData.mobile
+if (isMobile) {
+    card.innerHTML = ' <p class="big">w3bkn0t</p> <p>This websote is only compatible with mobile,</p> <p> go on your mobile device and try again.</p> '
+}
 const fileInput = document.getElementById('ipain');
 fileInput.addEventListener('change', (event) => {
     file = event.target.files;
     console.log(file[0]);
   });
-if (isMobile) {
-    card.innerHTML = ' <p class="big">w3bkn0t</p> <p>This websote is only compatible with mobile,</p> <p> go on your mobile device and try again.</p> '
-}
 
 function exploit() {
     console.log('the exploit should run now but there is not anything here')
@@ -23,6 +23,6 @@ function run() {
         card.innerHTML = '<p class="big">Running Exploit.</p>'
         exploit()
     } else {
-        card.innerHTML = '<p class="big"> ERROR: No File Seclected. </p> <br> <button onclick="reloadbtoh();"> <p>Back to home</p> </button>'
+        card.innerHTML = '<p class="big"> ERROR: No File Seclected. </p> <br> <button classs="button" onclick="reloadbtoh();"> <p>Back to home</p> </button>'
     }
 }
