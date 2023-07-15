9e34d21b3d4dd9d122f41a00d6ae4e9cf2a9a3ae
home-github <124263428+home-gihub@users.noreply.github.com>
2023-07-15 20:52:55 +0100

    add if mobile

--- js/index.js
+++ js/index.js
@@ -3,11 +3,15 @@ function reloadbtoh() {
     location.reload()
 }
 var file = ""
+const isMobile = navigator.userAgentData.mobile
 const fileInput = document.getElementById('ipain');
 fileInput.addEventListener('change', (event) => {
     file = event.target.files;
     console.log(file[0]);
   });
+if (isMobile) {
+    card.innerHTML = ' <p class="big">w3bkn0t</p> <p>This websote is only compatible with mobile,</p> <p> go on your mobile device and try again.</p> '
+}
 
 function exploit() {
     console.log('the exploit should run now but there is not anything here')
